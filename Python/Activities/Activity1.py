fadb903b13576baef43d726ce1d9a3c3179542c4
dbhalerao21 <79352923+dbhalerao21@users.noreply.github.com>
2021-08-21 17:49:15 +0530

    Add files via upload
    Added all 24 activities

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,10 @@
+	
+name = input( "What is your name: " )
+age=input( "How old are you: " ) 
+age1 =int(age)
+year = str( ( 2020 - age1 ) + 100 )
+print( name + " will be 100 years old in the year " + year )
+
+
+
+
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+#mod = num % 2
+if num % 2 == 0:
+    print("Entered no is even number.")
+else:
+    print("Entered no is odd number.")
