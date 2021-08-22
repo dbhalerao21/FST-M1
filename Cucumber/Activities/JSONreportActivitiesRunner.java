e71e822d2c0dad9ae22864e02944ade6e12f6843
dbhalerao21 <79352923+dbhalerao21@users.noreply.github.com>
2021-08-22 21:47:14 +0530

    Add files via upload

---
+++ Cucumber/Activities/JSONreportActivitiesRunner.java
@@ -0,0 +1,19 @@
+package cucumberTest;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@SimpleAlert",
+    plugin = {"json: test-reports/json-report.json"},
+    monochrome = true
+)
+
+public class JSONreportActivitiesRunner {
+
+}
